915e3533e01e3b0b154f7b398521897db1bc3e40
mateo <9461929@qq.com>
2023-10-16 00:14:48 +0800

    初始化

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ModifyLaunch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.hPort = new System.Windows.Forms.TextBox();
+            this.sPort = new System.Windows.Forms.TextBox();
+            this.hostip = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "补丁下载端口";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 10;
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ReshowDelay = 100;
+            this.toolTip1.ShowAlways = true;
+            this.toolTip1.StripAmpersands = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "提示";
+            // 
+            // hPort
+            // 
+            this.hPort.Location = new System.Drawing.Point(151, 106);
+            this.hPort.Name = "hPort";
+            this.hPort.Size = new System.Drawing.Size(167, 21);
+            this.hPort.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.hPort, "对应: 补丁下载端口");
+            // 
+            // sPort
+            // 
+            this.sPort.Location = new System.Drawing.Point(151, 146);
+            this.sPort.Name = "sPort";
+            this.sPort.Size = new System.Drawing.Size(167, 21);
+            this.sPort.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.sPort, "对应: 服务器端口");
+            // 
+            // hostip
+            // 
+            this.hostip.Location = new System.Drawing.Point(151, 68);
+            this.hostip.Name = "hostip";
+            this.hostip.Size = new System.Drawing.Size(167, 21);
+            this.hostip.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.hostip, "对应: 服务器地址");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "服务管理端口";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "服务器地址";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(131, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "登录器修改工具";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "作者QQ: 2807827464";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 259);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hostip);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hPort);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "登录器修改工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox hPort;
+        private System.Windows.Forms.TextBox sPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox hostip;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using Mono.Cecil.Cil;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModifyLaunch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 这是将字符串转换为整数的方法
+        private int StringToInt(string input)
+        {
+            int result;
+            if (int.TryParse(input, out result))
+            {
+                return result;
+            }
+            else
+            {
+                // 转换失败时的处理逻辑，可以是弹出消息框等
+                MessageBox.Show("输入无效，请输入一个有效的整数。");
+                return 0; // 或者你可以返回其他默认值
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 打开程序集
+                AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly("WowLaunchApp.wtf");
+
+                // 获取命名空间和类名
+                string namespaceName = "WowLaunchApp";
+                string className = "Utils";
+
+                // 获取要修改的方法名
+                string methodName = "InitUtils";
+
+                // 查找目标类
+                TypeDefinition targetClass = null;
+                foreach (ModuleDefinition module in assembly.Modules)
+                {
+                    foreach (TypeDefinition type in module.Types)
+                    {
+                        if (type.Namespace == namespaceName && type.Name == className)
+                        {
+                            targetClass = type;
+                            break;
+                        }
+                    }
+                }
+
+                // 找到目标类后进行操作
+                if (targetClass != null)
+                {
+                    // 查找目标方法
+                    MethodDefinition targetMethod = targetClass.Methods.FirstOrDefault(m => m.Name == methodName);
+                    if (targetMethod != null && targetMethod.HasBody)
+                    {
+                        // 获取方法体
+                        Mono.Cecil.Cil.MethodBody methodBody = targetMethod.Body;
+
+                        // 获取要修改的指令位置 // debug下标和releas不同, 需要调试
+                        Instruction host = methodBody.Instructions[0];
+                        Instruction socketPort = methodBody.Instructions[2];
+                        Instruction httpPort = methodBody.Instructions[4];
+                        int _httpPort = StringToInt(hPort.Text);
+                        int _SocketpPort = StringToInt(sPort.Text);
+                        // 创建新的指令
+                        Instruction new_host = Instruction.Create(OpCodes.Ldstr, hostip.Text);
+                        Instruction new_socketPort = Instruction.Create(OpCodes.Ldc_I4, _SocketpPort);
+                        Instruction new_httpPort = Instruction.Create(OpCodes.Ldc_I4, _httpPort);
+
+                        // 替换指令
+                        methodBody.GetILProcessor().Replace(host, new_host);
+                        methodBody.GetILProcessor().Replace(socketPort, new_socketPort);
+                        methodBody.GetILProcessor().Replace(httpPort, new_httpPort);
+                    }
+                }
+
+                // 保存修改后的程序集到新的文件
+                assembly.Write("登录器.exe");
+                MessageBox.Show("修改完成");
+                Application.Exit();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("修改失败...");
+            }
+            
+        }
+    }
+}
